bef02bbb1b5f7b8f8a605fa9eccec634b9e38ded
Leon Yip <lyip1992@gmail.com>
2015-07-10 19:36:35 -0700

    added routing for get requests to comments.json on server

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// app.get('/comments.json', function(req, res) {
-//   fs.readFile('comments.json', function(err, data) {
-//     res.setHeader('Cache-Control', 'no-cache');
-//     res.json(JSON.parse(data));
-//   });
-// });
-//
+app.get('/comments.json', function(req, res) {
+  fs.readFile('comments.json', function(err, data) {
+    res.setHeader('Cache-Control', 'no-cache');
+    res.json(JSON.parse(data));
+  });
+});
+
 // app.post('/comments.json', function(req, res) {
 //   fs.readFile('comments.json', function(err, data) {
 //     var comments = JSON.parse(data);
