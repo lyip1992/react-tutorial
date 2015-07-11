ac60196464c525967e9056a67ab07b86af19b76f
Leon Yip <lyip1992@gmail.com>
2015-07-10 19:20:02 -0700

    added mock data, allowed disabled escaping in markup, and made comment rendering dynamic

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,14 @@
+var data = [
+  { author: "Pete Hunt", text: "This is one comment" },
+  { author: "Jordan Walke", text: "This is *another* comment" }
+];
+
 var CommentBox = React.createClass({
   render: function(){
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={ this.props.data } />
         <CommentForm />
       </div>
     );
@@ -12,10 +17,16 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
   render: function(){
+    var commentNodes = this.props.data.map(function(comment){
+      return (
+        <Comment author={ comment.author }>
+          { comment.text }
+        </Comment>
+      );
+    });
     return (
       <div className="commentList">
-        <Comment author="Pete Hunt">This is one comment.</Comment>
-        <Comment author="Jordan Walke">This is *another* comment.</Comment>
+        { commentNodes }
       </div>
     );
   }
@@ -31,15 +42,16 @@ var CommentForm = React.createClass({
 
 var Comment = React.createClass({
   render: function(){
+    var rawMarkup = marked(this.props.children.toString(), { sanitize: true });
     return (
       <div className="comment">
         <h2 className="commentAuthor">
-          {this.props.author}
+          { this.props.author }
         </h2>
-        {this.props.children}
+      <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
       </div>
     );
   }
 });
 
-React.render(<CommentBox />, document.getElementById('content'));
+React.render(<CommentBox data={data}/>, document.getElementById('content'));
