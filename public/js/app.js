451e6ef1137656654d8bd5bb66e25b92c82b89fe
Leon Yip <lyip1992@gmail.com>
2015-07-10 19:39:48 -0700

    refactored app.js to poll for new comments every 2 seconds

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,5 @@
 var CommentBox = React.createClass({
-  getInitialState: function(){
-    return { data: [] };
-  },
-
-  componentDidMount: function(){
+  loadCommentsFromServer: function(){
     $.ajax({
       url: this.props.url,
       dataType: 'json',
@@ -15,6 +11,15 @@ var CommentBox = React.createClass({
         console.error(this.props.url, status, err.toString());
       }.bind(this)
     });
+  },
+
+  getInitialState: function(){
+    return { data: [] };
+  },
+
+  componentDidMount: function(){
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval)
   },
 
   render: function(){
@@ -67,4 +72,4 @@ var Comment = React.createClass({
   }
 });
 
-React.render(<CommentBox url="comments.json"/>, document.getElementById('content'));
+React.render(<CommentBox url="comments.json" pollInterval={ 2000 }/>, document.getElementById('content'));
